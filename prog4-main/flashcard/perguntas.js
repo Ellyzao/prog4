134b6341f0515f36562bc210d9d32f5af8e76934
Ellyzao <161478755+Ellyzao@users.noreply.github.com>
2024-11-19 09:40:14 -0300

    Add files via upload

---
+++ prog4-main/flashcard/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'hello kitty',
+    'quem criou a hello kitty?',
+    'yuko shimizu'
+)
+
+criaCartao(
+    'hello kitty',
+    'em que ano foi lançada ?',
+    'em 1974'
+)
+
+criaCartao(
+    'hellokitty',
+    'em que cidade hello kitty nasceu?',
+    'tóquio'
+)
+
+criaCartao(
+    'hello kitty',
+    'qual foi a inspiração para criar a hello kitty?',
+    'a inspiração veio de um gato branco chamado maneki-neko'
+)
